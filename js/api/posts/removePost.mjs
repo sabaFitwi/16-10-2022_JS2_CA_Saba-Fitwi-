7b59e0b3b162d8100e90680e1b06c49cbbdb3d22
sabaFitwi <fitwisaba@gmail.com>
2022-10-14 15:42:59 +0200

    commit

--- js/api/posts/removePost.mjs
+++ js/api/posts/removePost.mjs
@@ -1,24 +1,26 @@
 import { authFetch } from "../authFetch.mjs";
 import { postsApi } from "../fetchApi.mjs";
 import { id } from "../profile/singleProfile.mjs";
-
+console.log(id);
 //delete a post
+
 const postDelete = document.querySelector("#postDeleteButton");
 
 postDelete.addEventListener("click", () => {
   removePost(id);
+
   location.reload();
 });
 
-export async function removePost(id) {
+async function removePost(id) {
   if (!id) {
     throw new Error("remove requires a postID");
   }
-  const updatePostApi1 = postsApi + "/" + id;
+  const updatePostApi1 = postsApi + "/" + id + "?force=true";
   console.log(updatePostApi1);
 
   const response = await authFetch(updatePostApi1, {
-    method: "delete",
+    method: "delete" == true,
   });
   const result = await response.json();
   return result;
